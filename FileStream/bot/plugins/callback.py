65e6a8a6fe5943c48b59129d992eee96090c69d1
Silicon Developer <166461734+Silicon-Developer@users.noreply.github.com>
2024-08-18 10:54:33 +0530

    Update callback.py

--- FileStream/bot/plugins/callback.py
+++ FileStream/bot/plugins/callback.py
@@ -134,8 +134,8 @@ async def gen_file_menu(_id, file_list_no, update: CallbackQuery):
     else:
         file_type = "Unknown"
 
-    page_link = f"{Server.URL}watch/{myfile_info['_id']}"
-    stream_link = f"{Server.URL}dl/{myfile_info['_id']}"
+    page_link = f"{Telegram.STREAM_URL}?link={myfile_info['_id']}"
+    stream_link = f"{Telegram.DOWNLOAD_URL}?link={myfile_info['_id']}"
     if "video" in file_type.lower():
         MYFILES_BUTTONS = InlineKeyboardMarkup(
             [
