cb9ed1a0c78d0bbe02de7f1d66def7746583124f
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-08-21 21:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'GreyMatters'
-
+    return 'Vansh-Yadav'
 
 if __name__ == "__main__":
-    app.run()
+    # Get the port from the environment variable or default to 8080
+    port = int(os.environ.get("PORT", 8080))
+    # Run the app on the specified port and make it accessible externally
+    app.run(host='0.0.0.0', port=port)
